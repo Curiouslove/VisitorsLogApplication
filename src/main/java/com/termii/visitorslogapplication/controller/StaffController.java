360aaf54d63266f87119cba54aaa7002671f962a
Love Okum <l.okum@semicolon.africa>
2022-03-30 09:04:28 +0100

    updating endpoints

--- src/main/java/com/termii/visitorslogapplication/controller/StaffController.java
+++ src/main/java/com/termii/visitorslogapplication/controller/StaffController.java
@@ -1,15 +1,22 @@
 package com.termii.visitorslogapplication.controller;
 
+import com.termii.visitorslogapplication.data.model.Staff;
 import com.termii.visitorslogapplication.dto.request.LoginDto;
 import com.termii.visitorslogapplication.dto.request.NewVisitRequest;
 import com.termii.visitorslogapplication.dto.request.StaffDto;
+import com.termii.visitorslogapplication.service.StaffService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@AllArgsConstructor
 @RestController
 @RequestMapping("/api/")
 public class StaffController {
+    private StaffService staffService;
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginDto loginDto){
@@ -28,11 +35,14 @@ public class StaffController {
 
     @GetMapping("/staff")
     public ResponseEntity<?> getAllStaff(){
+       List<Staff> staffList = staffService.getAllStaff();
+       return new ResponseEntity<>(staffList, HttpStatus.OK);
 
     }
 
     @GetMapping("/staff/{id}")
     public ResponseEntity<?> getSpecificStaff(@PathVariable Long id){
+
 
     }
 
